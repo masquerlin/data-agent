414fc31088714e6a3bd060bf82057eb00c25ec75
wangdalin <731994586@qq.com>
2024-03-19 17:42:30 +0800

    Encapsulate class

--- config.py
+++
@@ -1,7 +0,0 @@
-import argparse
-def get_args():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--user_query", type=str,
-                        default="", help="openai-keys")
-        args = parser.parse_args()
-        return args
